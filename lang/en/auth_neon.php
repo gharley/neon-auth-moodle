5250149058259f09315364662cfaea3adbed1add
Greg Harley <greg@harleyhood.com>
2016-08-22 12:01:37 -0600

    Added template files from auth_none

---
+++ lang/en/auth_neon.php
@@ -0,0 +1,16 @@
+<?php
+// This file is a custom plugin for Moodle - http://moodle.org/
+//
+// This software is Copyright(c) 2016 by Greg Harley
+// License - MIT https://opensource.org/licenses/MIT MIT
+
+/**
+ * Strings for component 'auth_neon', language 'en'.
+ *
+ * @package   auth_neon
+ * @copyright 2016 onwards Greg Harley
+ * @license   https://opensource.org/licenses/MIT MIT
+ */
+
+$string['auth_neondescription'] = 'Users can sign in using their Neon CRM account.';
+$string['pluginname'] = 'Neon CRM';
